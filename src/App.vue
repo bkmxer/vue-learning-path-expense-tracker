7d0bd5d294f6f167d2b3043607ae2aa51172d4a1
Anton Ilchuk <ilchuk.anton@gmail.com>
2024-03-09 23:29:29 +0000

    Added the localStorage support to save the changes

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,18 @@ import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
 import {useToast} from 'vue-toastification';
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
 const toast = useToast();
-const transactions = ref([
-  { id: 1, text: 'Flower', amount: -19.99 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
-]);
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
 
 // Get total
 const total = computed(() => {
@@ -59,6 +62,8 @@ const handleTransactionSubmitted = (transactionData) => {
     amount: +transactionData.amount
   });
 
+  saveToLocalStorage();
+
   toast.success('Transaction added');
 }
 
@@ -70,6 +75,14 @@ const generateTransactionId = () => {
 // Handles the deletion of a transaction with the given id from the transactions array.
 const handleTransactionDeleted = (id) => {
   transactions.value = transactions.value.filter(transaction => transaction.id !== id)
+
+  saveToLocalStorage();
+
   toast.success('Transaction removed');
+}
+
+// Saves the 'transactions' value to the local storage as a JSON string.
+const saveToLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
 }
 </script>
